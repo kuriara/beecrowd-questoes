374e2e86605c285bc27bc736086e3b6d7cd84312
kurihara <takaokuriharagomes@gmail.com>
2026-03-06 20:24:51 -0300

    feature: questao 1036 beecrowd
    questao 1036 beecrowd resolvida

--- formulaDeBhaskara_1036.py
+++ formulaDeBhaskara_1036.py
@@ -1,1 +1,17 @@
-a = int(input())
+from math import sqrt
+
+a, b, c = input().split()
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+delta = (b**2.0) - (4.0 * a * c)
+
+if a == 0 or delta < 0:
+    print("Impossivel calcular")
+else:
+    raizPositiva = ((-b) + sqrt(delta)) / (2.0 * a)
+    raizNegativa = ((-b) - sqrt(delta)) / (2.0 * a)
+    print(f"R1 = {raizPositiva:.5f}\nR2 = {raizNegativa:.5f}")
+
