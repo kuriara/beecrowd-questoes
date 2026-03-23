27612c9f7beb33b648ee06247811a217ddd27aa0
kurihara <takaokuriharagomes@gmail.com>
2026-03-22 23:09:24 -0300

    feature questao 1051 beecrowd
    questao 1051 beecrowd resolvida

---
+++ imposto_de_renda_1051.py
@@ -0,0 +1,39 @@
+# renda = float(input())
+
+# if renda <= 2000.00:
+#     print("Isento")
+
+# if renda > 4500.00:
+#     renda_28 = (renda % 4500) * 28 / 100
+#     renda_18 = (renda % 3000 - renda % 4500) * 18 / 100 
+#     renda_8 = (renda % 2000 - renda % 3000) * 8 / 100
+#     renda = renda_28 + renda_18 + renda_8
+#     print(renda)
+
+# elif renda > 3000.00 and renda <= 4500.00:
+#     renda_18 = (renda % 3000) * 18 / 100
+#     renda_8 = (renda % 2000 - renda % 3000) * 8 / 100
+#     renda = renda_18 + renda_8
+#     print(renda)
+
+renda = float(input())
+
+if renda <= 2000.00:
+    print("Isento")
+
+elif renda > 4500.00:
+    renda_28 = (renda - 4500) * 28 / 100
+    renda_18 = (renda - 3000 - (renda - 4500)) * 18 / 100
+    renda_8 = (renda - 2000 - (renda - 3000)) * 8 / 100
+    renda = renda_28 + renda_18 + renda_8
+    print(renda)
+
+elif renda > 3000.00 and renda <= 4500.00:
+    renda_18 = (renda - 3000) * 18 / 100
+    renda_8 = (renda - 2000 - (renda - 3000)) * 8 / 100
+    renda = renda_18 + renda_8
+    print(renda)
+
+elif renda > 2000.00 and renda <= 3000.00:
+    renda_8 = (renda - 2000) * 8 / 100
+    print(renda_8)
