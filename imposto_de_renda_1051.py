e8426f068bb732471017093b37c121f8b04a6dea
kurihara <takaokuriharagomes@gmail.com>
2026-03-28 00:43:28 -0300

    feature questao 1051 beecrowd
    questao 1051 beecrowd resolvida

--- imposto_de_renda_1051.py
+++ imposto_de_renda_1051.py
@@ -26,14 +26,14 @@ elif renda > 4500.00:
     renda_18 = (renda - 3000 - (renda - 4500)) * 18 / 100
     renda_8 = (renda - 2000 - (renda - 3000)) * 8 / 100
     renda = renda_28 + renda_18 + renda_8
-    print(renda)
+    print(f"R$ {renda:.2f}")
 
 elif renda > 3000.00 and renda <= 4500.00:
     renda_18 = (renda - 3000) * 18 / 100
     renda_8 = (renda - 2000 - (renda - 3000)) * 8 / 100
     renda = renda_18 + renda_8
-    print(renda)
+    print(f"R$ {renda:.2f}")
 
 elif renda > 2000.00 and renda <= 3000.00:
-    renda_8 = (renda - 2000) * 8 / 100
-    print(renda_8)
+    renda = (renda - 2000) * 8 / 100
+    print(f"R$ {renda:.2f}")
