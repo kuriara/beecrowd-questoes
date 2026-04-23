d9896ee5f1ec6f63cdacd344462651b1730cea26
kurihara <takaokuriharagomes@gmail.com>
2026-04-23 18:58:52 -0300

    feature questao 1133 beecrowd
    questao resolvida 1133 beecrowd

---
+++ resto_da_divisao_1133.py
@@ -0,0 +1,17 @@
+texto = ""
+
+for i in range (2):
+    n = int(input())
+    if i == 0:
+        n1 = n
+    else:
+        if n < n1:
+            for i in range(n+1, n1):
+                if i % 5 == 2 or i % 5 == 3:
+                    texto = texto + f"{str(i)}\n"
+        elif n1 < n:
+            for i in range(n1+1, n):
+                if i % 5 == 2 or i % 5 == 3:
+                    texto = texto + f"{str(i)}\n"
+
+print(texto, end="")
