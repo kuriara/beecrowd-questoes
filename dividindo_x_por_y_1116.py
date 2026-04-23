3431b35d58e7023c44c9280309f37d8b7f06115c
kurihara <takaokuriharagomes@gmail.com>
2026-04-23 18:58:07 -0300

    feature questao 1116 beecrowd
    questao resolvida 1116 beecrowd

--- dividindo_x_por_y_1116.py
+++ dividindo_x_por_y_1116.py
@@ -1,18 +1,15 @@
+n = int(input())
 texto = ""
 
-for i in range (2):
-    n = int(input())
-    if i == 0:
-        n1 = n
+for i in range(n):
+    x, y = input().split()
+    x, y = float(x), float(y)
+    try:
+        divisao = x / y
+    except ZeroDivisionError:
+        texto = texto + "divisao impossivel\n"
     else:
-        if n < n1:
-            for i in range(n+1, n1):
-                if i % 5 == 2 or i % 5 == 3:
-                    texto = texto + f"{str(i)}\n"
-        elif n1 < n:
-            for i in range(n1+1, n):
-                if i % 5 == 2 or i % 5 == 3:
-                    texto = texto + f"{str(i)}\n"
+        texto = f"{texto}{divisao:.1f}\n"
 
 print(texto, end="")
         
