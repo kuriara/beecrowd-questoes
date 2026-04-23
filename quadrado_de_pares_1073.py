3acb8342da24cf0ff3e4b972362ce3d457d59b54
kurihara <takaokuriharagomes@gmail.com>
2026-04-23 15:39:26 -0300

    feature questao 1073 beecrowd
    questao 1073 resolvida beecrowd

--- quadrado_de_pares_1073.py
+++ quadrado_de_pares_1073.py
@@ -5,7 +5,7 @@ mensagem = ""
 while contador <= n:
     if contador % 2 == 0:
         valor = contador ** 2
-        somatorio = f"{contador}^2={valor}\n"
+        somatorio = f"{contador}^2 = {valor}\n"
         mensagem = mensagem + somatorio
     contador+=1
 print(mensagem, end="")
