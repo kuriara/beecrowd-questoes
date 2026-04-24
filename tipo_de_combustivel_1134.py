e32f6ede87b2f3419dca5e5123f34cf752d5d362
kurihara <takaokuriharagomes@gmail.com>
2026-04-24 18:00:05 -0300

    feature questao beecrowd 1134
    questao resolvida beecrowd 1134

--- tipo_de_combustivel_1134.py
+++ tipo_de_combustivel_1134.py
@@ -1,17 +1,21 @@
 def combustivel(codigo):
     alcool = gasolina = diesel = 0
+
     while True:
-        if codigo == 1:
-            alcool += 1
-        elif codigo == 2:
-            gasolina += 1
-        elif codigo == 3:
-            diesel += 1
-        elif codigo == 4:
-            return f"MUITO OBRIGADO\nAlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}"
+        if codigo >= 1 and codigo <= 4:
+            while True:
+                if codigo == 1:
+                    alcool += 1
+                elif codigo == 2:
+                    gasolina += 1
+                elif codigo == 3:
+                    diesel += 1
+                elif codigo == 4:
+                    return f"MUITO OBRIGADO\nAlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}"
+                codigo = int(input())
         else:
             codigo = int(input())
-        codigo = int(input())
+
 
 def main():
     codigo = int(input())
