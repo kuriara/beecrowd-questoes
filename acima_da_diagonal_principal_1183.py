f03c3dd5f52e62bcb38193b7330402c56718ccb3
kurihara <takaokuriharagomes@gmail.com>
2026-06-07 19:31:51 -0300

    Update acima_da_diagonal_principal_1183.py

--- acima_da_diagonal_principal_1183.py
+++ acima_da_diagonal_principal_1183.py
@@ -1,14 +1,25 @@
-matriz = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+matriz = []
 
 soma = 0
+contador = 0
+operacao = input()
+
+for i in range(12):
+    lista = []
+    for j in range(12):
+        numero = float(input())
+        lista.append(numero)
+    matriz.append(lista)
+
 
 for i in range(len(matriz)):
     for j in range(len(matriz[i])):
         if j > i:
             soma = soma + matriz[i][j]
+            contador += 1
 
-print(soma)
+if operacao == "S":
+    print(soma)
+
+if operacao == "M":
+    print(f"{soma / contador:.1f}")
