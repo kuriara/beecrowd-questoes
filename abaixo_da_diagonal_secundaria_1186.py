63f497adbf3e1979a78ed828f03024d8ceb86ac4
kurihara <takaokuriharagomes@gmail.com>
2026-06-07 21:28:13 -0300

    feature

--- abaixo_da_diagonal_secundaria_1186.py
+++ abaixo_da_diagonal_secundaria_1186.py
@@ -1,11 +1,14 @@
-matriz = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+matriz = []
 
 soma = contador = 0
 operacao = input()
+
+for i in range(12):
+    lista = []
+    for j in range(12):
+        numero = float(input())
+        lista.append(numero)
+    matriz.append(lista)
 
 for i in range(len(matriz)):
     for j in range(len(matriz[i])):
@@ -15,3 +18,5 @@ for i in range(len(matriz)):
 
 if operacao == "S":
     print(soma)
+if operacao == "M":
+    print(f"{soma / contador:.1f}")
