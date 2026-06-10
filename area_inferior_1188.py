22667cce494c03319a5ad5c9e42cb9c53588037c
kurihara <takaokuriharagomes@gmail.com>
2026-06-10 19:40:11 -0300

    feature questao beecrowd 1188
    questao resolvida beecrowd 1188

---
+++ area_inferior_1188.py
@@ -0,0 +1,27 @@
+matriz = []
+
+soma = contador = 0
+
+operacao = input()
+
+for i in range(12):
+    lista = []
+    for j in range(12):
+        numero = float(input())
+        lista.append(numero)
+    matriz.append(lista)
+
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        if i + j > len(matriz) - 1 and i > j:
+            soma = soma + matriz[i][j]
+            contador += 1
+
+if operacao == "S":
+    print(f"{soma:.1f}")
+
+if operacao == "M":
+    print(f"{soma / contador:.1f}")
+
+    
